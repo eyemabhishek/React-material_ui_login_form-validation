b21bccb986c736c9aca3693d7188c648142b7be8
Abhishek <eyemabhishek@gmail.com>
2021-07-11 14:23:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import SignInOutControl from "./container";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      
+      <SignInOutControl />
+    </div>
+  );
+}
+
+export default App;
